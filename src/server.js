91a8b938ac55c1010b27232989ba10cd8c0855ba
Boris Bilichenko <boris.bilichenko@gmail.com>
2022-11-29 16:07:36 +0200

    Configure server to host any files. Adjust .gitignore to not track .yarn and node_modules files

--- src/server.js
+++ src/server.js
@@ -1,12 +1,64 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('./src/index.html').pipe(res)
+const server = http.createServer((request, response) => {
+  console.log('Request starting...');
+
+    let filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    const relativeFilePath = path.resolve(__dirname, filePath);
+    fs.readFile(relativeFilePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                console.error("Error, file not found", relativeFilePath);
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                console.error('Unknown error', error)
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            console.log('Request finished!')
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
 });
 
 server.listen(port, hostname, () => {
